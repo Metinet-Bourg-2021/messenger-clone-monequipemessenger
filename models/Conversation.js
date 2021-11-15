0f6fed930a3a22e7a776fde386a9fb4595bed00b
BDMaxiTacos <tristan.tsti2d5@gmail.com>
2021-11-15 12:05:44 +0100

    shemas conversation & message

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
+const Message = require('./Message');
+const User = require('./User')
 
-const conversationSchema = mongoose.Schema({}, { minimize: false });
+const conversationSchema = mongoose.Schema({
+    type_conv: {
+        type: String
+    },
+    participants: {
+        type: [User]
+    },
+    messages: {
+        type: [Message]
+    },
+    title: {
+        type: String
+    },
+    theme: {
+        type: String
+    },
+    updated_at: {
+        type: Date
+    },
+    seen: {
+        type: [User]
+    },
+    typing: {
+        type: [User]
+    }
+
+
+    // "id":1,
+    // "type":"one_to_one",
+    // "participants": ["John", "Jane"],
+    // "messages": [{...}],
+    // "title": "Cmaconversation",
+    // "theme": "BLUE",
+    // "updated_at": "1995-12-17T03:24:00",
+    // "seen": {},
+    // "typing": {}
+}, { minimize: false });
 
 module.exports = mongoose.model("Conversation", conversationSchema);
