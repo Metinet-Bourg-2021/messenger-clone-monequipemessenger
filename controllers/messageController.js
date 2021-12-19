6dd70f950c9b9d3743ae80e2912bac91106d1fd1
Stan Devs <atrox.cod@gmail.com>
2021-12-19 18:06:46 +0100

    refacto

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,38 +1,49 @@
+const res = require("express/lib/response");
+const jwt = require("jsonwebtoken");
+const checkAuth = require("../auth");
+const { SUCCESS, NOT_FOUND_USER } = require("../codes");
 const Conversation = require("../models/Conversation");
 const Message = require("../models/Message");
+const User = require("../models/User");
 
-const saveMessage = ({ token, conversation_id, content }) => {
-  console.log(content, conversation_id);
-  const newMessage = new Message({
-    from: "LeStan2",
-    content: content,
-    posted_at: Date.now(),
-    delivered_to: [],
-    reply_to: null,
-    edited: false,
-    deleted: false,
-    reactions: {},
-  });
+const saveMessage = async ({ token, conversation_id, content }, callback) => {
+  if (!checkAuth(token)) return callback({ code: NOT_AUTHENTICATED, data: {} });
 
-  newMessage.save().then((message) =>
-    Conversation.findOne({ _id: conversation_id }).then((conversation) => {
-      const newMessages = [...conversation.messages, message._id];
-      Conversation.updateOne(
-        { _id: conversation._id },
-        {
-          messages: newMessages,
-        }
-      ).then((conv) => ""); // whaaaat
-    })
-  );
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    const userOfToken = await User.findById(decodedToken.userId);
+    if (!userOfToken) return callback({ code: NOT_FOUND_USER });
 
-  //   newMessage
-  //     .save()
-  //     .then((message) =>
-  //       Message.populate(newMessage, { path: "conversation" }).then((mess) =>
-  //         console.log(mess)
-  //       )
-  //     );
+    const conversation = await Conversation.findOne({ _id: conversation_id });
+    if (conversation) {
+      const createdMessage = await Message.create({
+        from: userOfToken.username,
+        content: content,
+        posted_at: Date.now(),
+        delivered_to: [],
+        reply_to: null,
+        edited: false,
+        deleted: false,
+        reactions: {},
+      });
+
+      const newMessagesOfConversation = [
+        ...conversation.messages,
+        createdMessage._id,
+      ];
+      await Conversation.updateOne(
+        { _id: conversation_id },
+        { messages: newMessagesOfConversation }
+      );
+
+      return callback({
+        code: SUCCESS,
+        data: { message: { ...createdMessage._doc, id: createdMessage._id } },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 module.exports = { saveMessage };
